e745c1cd42f6632fa56edce63b8f1b29bea0e3f1
Xavier <chabolaxavi@gmail.com>
2023-05-03 17:59:04 +0200

    Clase instancia actualizada

--- gestionIncidencias/src/gestionIncidencias/Incidencia.java
+++ gestionIncidencias/src/gestionIncidencias/Incidencia.java
@@ -67,48 +67,8 @@ public class Incidencia {
 	
 	// MÉTODOS
 	
-	public static Incidencia registrarIncidencia(int puesto, String descripcion, ArrayList<Incidencia> arrayListIncidencias) {
-		
-		// Instanciamos una nueva incidencia y la añadimos a la lista de incidencias.
-		Incidencia nuevaInc = new Incidencia(puesto, descripcion);
-		arrayListIncidencias.add(nuevaInc);
-		
-		return nuevaInc;
-	}
-	
-	public static void resolverIncidencia(int id, String solucion, ArrayList<Incidencia> arrayListIncidencias) {
-		
-		// Iteramos a través del arraylist de incidencias en busca de la incidencia con el id de la incidencia que queremos resolver. 
-		// Una vez la encontramos actualizamos su estado a "Resuelto" y hacemos lo mismo añadiendo la solución que pediremos en el programa principal.
-		for (Incidencia incidencia : arrayListIncidencias) {
-			if (incidencia.getId() == id) {
-				incidencia.setEstado("Resuelto");
-				incidencia.setSolucion(solucion);
-				break;
-			}
-		}		
-	}
-	
-	public static void mostrarIncidencias(ArrayList<Incidencia> arrayListIncidencias) {
-		
-		// Iteramos a través del arraylist de incidencias y vamos mostrando los atributos de cada incidencia, antes comprobando si su estado es "Pendiente", 
-		// en cuyo caso no imprimiremos su atributo "solucion", ya que está vacío; en caso contrario, sí imprimiremos todos sus atributos.
-		for (Incidencia incidencia : arrayListIncidencias) {
-			if (incidencia.getEstado() == "Pendiente") {
-				System.out.println("Incidencia: " + incidencia.getId() +
-								   " - Puesto: " +  incidencia.getPuesto() +
-								   " - " + incidencia.getDescripcion() + 
-								   " - Estado: " +  incidencia.getEstado()
-								   );
-			}
-			else {
-				System.out.println("Incidencia: " + incidencia.getId() +
-								   " - Puesto: " +  incidencia.getPuesto() +
-								   " - " + incidencia.getDescripcion() + 
-								   " - Estado: " +  incidencia.getEstado() +
-								   " - " + incidencia.getSolucion()
-								   );
-			}
-		}		
+	public void resolverIncidencia(Incidencia incidencia, String solucion) {
+		incidencia.setEstado("Resuelto");
+		incidencia.setSolucion(solucion);		
 	}
 }
